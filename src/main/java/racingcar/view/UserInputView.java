720e72920ca2ff1aaf9d4a8ed92892f9d60314c9
배재훈 <lange50300@gmail.com>
2025-10-26 17:51:55 +0900

    feat: 콘솔 입력 안내 메세지 및 입력 구현
    - 사용자에게 초기 안내 메세지를 출력합니다.
    - 사용자에게 입력값을 입력 받습니다.

---
+++ src/main/java/racingcar/view/UserInputView.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInputView {
+
+    public void getCarsNameMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void getAttemptNumberMessage() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+    }
+
+    public String getInput() {
+        return Console.readLine();
+    }
+}
