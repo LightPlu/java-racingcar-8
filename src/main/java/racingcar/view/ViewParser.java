1526ac8b1cb49db6507af6113c04558e1aca2b4d
배재훈 <lange50300@gmail.com>
2025-10-26 17:59:54 +0900

    feat: 출력값 파싱 구현
    - 각 자동차의 진행 현황을 파싱합니다.
    - 각 라운드 별 자동차들의 진행 현황을 묶습니다.
    - 최종 우승자에 대한 이름을 파싱합니다.

---
+++ src/main/java/racingcar/view/ViewParser.java
@@ -0,0 +1,28 @@
+package racingcar.view;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.RacingCar;
+
+public class ViewParser {
+
+    public static String parseCarProgress(RacingCar car) {
+        String progress = "-".repeat(car.getPosition());
+
+        return car.getName() + " : " + progress;
+    }
+
+    public static String parseRoundResult(List<RacingCar> car) {
+
+        return car.stream()
+                .map(ViewParser::parseCarProgress)
+                .collect(Collectors.joining("\n"));
+    }
+
+    public static String parseFinalWinner(List<RacingCar> winningCars) {
+
+        return winningCars.stream()
+                .map(RacingCar::getName)
+                .collect(Collectors.joining(", "));
+    }
+}
