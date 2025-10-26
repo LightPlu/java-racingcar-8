78d2543123bf18b6617ddb74cf4ba62a12ae08f1
배재훈 <lange50300@gmail.com>
2025-10-26 23:32:55 +0900

    refactor: 출력 메서드명 수정 및 출력 메서드 추가
    - 결과값 출력 메서드의 메서드명을 수정합니다.
    - 실행 결과에 대한 출력 메서드를 구현합니다.

--- src/main/java/racingcar/view/UserOutputView.java
+++ src/main/java/racingcar/view/UserOutputView.java
@@ -1,17 +1,22 @@
 package racingcar.view;
 
 import java.util.List;
-import racingcar.RacingCar;
+import racingcar.domain.car.RacingCar;
 
 public class UserOutputView {
 
-    public void showLevelResult(List<RacingCar> racingCars) {
+    public void showRoundResult(List<RacingCar> racingCars) {
         String roundResult = ViewParser.parseRoundResult(racingCars);
+        System.out.println(roundResult);
         System.out.println();
     }
 
     public void showFinalWinner(List<RacingCar> racingCars) {
         System.out.println("최종 우승자 : " + ViewParser.parseFinalWinner(racingCars));
+    }
+
+    public void showResultMessage() {
+        System.out.println("실행 결과");
     }
 
 }
