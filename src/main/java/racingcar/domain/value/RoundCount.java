3e7086ffc0a5532320c79c1a4c79abbe20934324
배재훈 <lange50300@gmail.com>
2025-10-27 13:27:29 +0900

    feat: RoundCount 클래스 검증 로직 추가
    - 시도할 횟수 입력에 공백이 있다면 예외를 발생시킵니다.

--- src/main/java/racingcar/domain/value/RoundCount.java
+++ src/main/java/racingcar/domain/value/RoundCount.java
@@ -5,15 +5,22 @@ public class RoundCount {
     private final int totalRound;
 
     public RoundCount(String totalRound) {
+        validateTrim(totalRound);
         validateInteger(totalRound);
         validatePositive(totalRound);
         this.totalRound = Integer.parseInt(totalRound);
     }
 
+    public void validateTrim(String input) {
+        if(input.startsWith(" ") || input.endsWith(" ")) {
+            throw new IllegalArgumentException("횟수 앞 뒤로 공백이 올 수 없습니다.");
+        }
+    }
+
     public void validatePositive(String input) {
         int validateValue = Integer.parseInt(input);
         if (validateValue < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("시도할 횟수에는 음수가 올 수 없습니다.");
         }
     }
 
@@ -21,7 +28,7 @@ public class RoundCount {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("시도할 횟수를 정수로 입력해주세요.");
         }
     }
 
