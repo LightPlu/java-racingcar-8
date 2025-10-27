2a958ab6df2f0587d7e8d39a1d0c3ce806152ebe
배재훈 <lange50300@gmail.com>
2025-10-27 10:11:20 +0900

    feat: 자동차 이동 조건 구현
    - 과제 요구사항에 맞춰 랜덤값이 4이상일 때 자동차가 이동합니다.

---
+++ src/main/java/racingcar/domain/policy/MovePolicy.java
@@ -0,0 +1,12 @@
+package racingcar.domain.policy;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class MovePolicy {
+
+    private final static int MOVE_THRESHOLD = 4;
+
+    public boolean carMoveCondition() {
+        return Randoms.pickNumberInRange(0,9) >= MOVE_THRESHOLD;
+    }
+}
